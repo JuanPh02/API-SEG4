98613bdba17061580831358014ac9c0de5e1749e
Juan Pablo Arroyave <50724493+JuanPh2002@users.noreply.github.com>
2021-05-05 10:29:11 -0500

    Nodemailer - mailer.ts

--- readme.md
+++ readme.md
@@ -10,6 +10,7 @@
 * https://expressjs.com/es/ - Exprees JS - 'npm install express'
 * https://www.npmjs.com/package/nodemon - Nodemon - 'npm install -g nodemon'
 * https://node-postgres.com - Node Postgres - 'npm install pg'
+* https://nodemailer.com/ - Nodemailer - 'npm install nodemailer'
 
 ## Desarrollada por:
 * Juan Pablo Arroyave 
--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ const app = express();
 const router = require('./routes/index');
 const PORT = 3001;
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 app.use('/', router);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.listen(PORT, () => {
     console.log(`Server started on port: http://localhost:${PORT}`);
